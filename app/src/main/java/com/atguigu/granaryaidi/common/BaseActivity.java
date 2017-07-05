8a38a57714568457d7a4838faddf4ccf28ded3dc
lianshuideyu <834893479@qq.com>
2017-07-05 21:40:53 +0800

    baseActivity的抽取，欢迎页面

---
+++ app/src/main/java/com/atguigu/granaryaidi/common/BaseActivity.java
@@ -0,0 +1,53 @@
+package com.atguigu.granaryaidi.common;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+
+/**
+ * Created by Administrator on 2017/7/5.
+ */
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(getLayoutId());
+
+        ButterKnife.inject(this);
+
+        initView();
+
+        initData();
+
+        initListener();
+    }
+
+    public abstract void initListener();
+
+    public abstract void initData();
+
+    public abstract void initView();
+
+    /**
+     * 添加布局id
+     * @return
+     */
+    public abstract int getLayoutId() ;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        ButterKnife.reset(this);
+    }
+
+    public void showToast(String message){
+        Toast.makeText(BaseActivity.this, message, Toast.LENGTH_SHORT).show();
+    }
+
+}
---
+++ app/src/main/java/com/atguigu/granaryaidi/view/SplashActivity.java
@@ -0,0 +1,70 @@
+package com.atguigu.granaryaidi.view;
+
+import android.content.Intent;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.atguigu.granaryaidi.MainActivity;
+import com.atguigu.granaryaidi.R;
+import com.atguigu.granaryaidi.common.BaseActivity;
+
+import butterknife.InjectView;
+
+public class SplashActivity extends BaseActivity {
+
+    @InjectView(R.id.splash_iv)
+    ImageView splashIv;
+    @InjectView(R.id.activity_splash)
+    RelativeLayout activitySplash;
+
+    private AlphaAnimation animation;
+    @Override
+    public void initListener() {
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                //动画结束跳转到主页面
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    @Override
+    public void initData() {
+
+    }
+
+    @Override
+    public void initView() {
+        startaAnimation();
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_splash;
+    }
+
+
+    private void startaAnimation() {
+        animation = new AlphaAnimation(0f, 1.0f);
+        animation.setDuration(2000);
+        animation.setFillAfter(true);
+
+        splashIv.startAnimation(animation);
+    }
+}
