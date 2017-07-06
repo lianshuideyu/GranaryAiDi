ddc73c48606fca5d017000c7317f28cf82886023
lianshuideyu <834893479@qq.com>
2017-07-07 00:16:39 +0800

    商店模块 ；； 首页页面点击右下角 图标 回到顶部

--- app/src/main/java/com/atguigu/granaryaidi/view/fragment/shopfragment/HomeFragment.java
+++ app/src/main/java/com/atguigu/granaryaidi/view/fragment/shopfragment/HomeFragment.java
@@ -1,10 +1,11 @@
 package com.atguigu.granaryaidi.view.fragment.shopfragment;
 
-import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -33,10 +34,16 @@ public class HomeFragment extends BaseFragment {
     ProgressBar pbShopHome;
     @InjectView(R.id.tv_shop_home)
     TextView tvShopHome;
+    @InjectView(R.id.ib_top)
+    ImageButton ibTop;
 
 
     private List<ShopHomeBean.DataBean.ItemsBean.ListBean> lists;
     private HomeAdapter adapter;
+    /**
+     * RecyclerView的布局管理器
+     */
+    private GridLayoutManager manager;
 
     /**
      * 联网获取数据
@@ -50,8 +57,7 @@ public class HomeFragment extends BaseFragment {
     @Override
     protected void initView() {
 
-        //设置RecyclerView的布局模式
-        rvShopHome.setLayoutManager(new LinearLayoutManager(context));
+
     }
 
     @Override
@@ -67,18 +73,15 @@ public class HomeFragment extends BaseFragment {
         HttpUtils.getInstance().get(url, new HttpUtils.MyHttpClickListener() {
             @Override
             public void onSuccess(String content) {
-                Log.e("home","联网成功==" + content);
-                if(!TextUtils.isEmpty(content)) {
+                Log.e("home", "联网成功==" + content);
+                if (!TextUtils.isEmpty(content)) {
                     //解析数据
                     processData(content);
-
                 }
-
             }
-
             @Override
             public void onFailure(String content) {
-                Log.e("home","联网失败==" + content);
+                Log.e("home", "联网失败==" + content);
             }
         });
     }
@@ -91,16 +94,36 @@ public class HomeFragment extends BaseFragment {
 
         lists = bean.getData().getItems().getList();
 
-        Log.e("home","解析==" + bean.getData().getItems().getList().get(0).getOne().getTopic_name());
+        Log.e("home", "解析==" + bean.getData().getItems().getList().get(0).getOne().getTopic_name());
 
-        if(lists != null && lists.size() > 0) {
+        if (lists != null && lists.size() > 0) {
 
-            adapter = new HomeAdapter(context,lists);
+            adapter = new HomeAdapter(context, lists);
 //            //设置适配器
             rvShopHome.setAdapter(adapter);
             //添加数据
 
-        }else {
+            //设置RecyclerView的布局模式
+            manager = new GridLayoutManager(context,1);
+            /**
+             * 通过GridLayoutManager监听recyclerview的位置变化
+             */
+            manager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+                @Override
+                public int getSpanSize(int position) {
+                    Log.e("home","setSpanSizeLookup==" + position);
+                    if(position <= 2) {
+                        ibTop.setVisibility(View.GONE);
+                    }else {
+                        ibTop.setVisibility(View.VISIBLE);
+                    }
+
+                    //只能返回1
+                    return 1;
+                }
+            });
+            rvShopHome.setLayoutManager(manager);
+        } else {
             //么有数据
             tvShopHome.setVisibility(View.VISIBLE);
         }
@@ -112,6 +135,15 @@ public class HomeFragment extends BaseFragment {
     @Override
     protected void initListener() {
 
+        /**
+         * 回到顶部图标的点击事件
+         */
+        ibTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rvShopHome.scrollToPosition(0);//回到顶部
+            }
+        });
     }
 
 
