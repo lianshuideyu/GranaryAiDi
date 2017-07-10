0b53f1e54bec00715f916f103d78d093e3956fd9
lianshuideyu <834893479@qq.com>
2017-07-11 00:34:55 +0800

    购买选项页，选项页的数据完善

--- app/src/main/java/com/atguigu/granaryaidi/view/Activity/GoodsGotoBuyActivity.java
+++ app/src/main/java/com/atguigu/granaryaidi/view/Activity/GoodsGotoBuyActivity.java
@@ -1,6 +1,9 @@
 package com.atguigu.granaryaidi.view.Activity;
 
+import android.graphics.Color;
 import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.text.TextUtils;
 import android.transition.Transition;
 import android.transition.TransitionInflater;
@@ -12,8 +15,11 @@ import android.widget.TextView;
 import com.atguigu.granaryaidi.Base.BaseActivity;
 import com.atguigu.granaryaidi.R;
 import com.atguigu.granaryaidi.bean.GoodsDetailsBean;
+import com.atguigu.granaryaidi.utils.DensityUtil;
 import com.atguigu.granaryaidi.view.viewmyself.AddSubView;
 import com.bumptech.glide.Glide;
+
+import java.util.List;
 
 import butterknife.InjectView;
 import butterknife.OnClick;
@@ -68,6 +74,7 @@ public class GoodsGotoBuyActivity extends BaseActivity {
 
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
     @Override
     public void initData() {
 
@@ -86,12 +93,86 @@ public class GoodsGotoBuyActivity extends BaseActivity {
             }else {
                 tvPrice.setText(items.getDiscount_price());
             }
-
+            //第一行的类型先设置了后边动态new
             tvProductType.setText(items.getSku_info().get(0).getType_name());
 
-//            llProductType
+//            设置可购买的类型
+            setllProductTypeData();
         }
 
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    private void setllProductTypeData() {
+
+        //放第一种类型的数据
+        LinearLayout linearLayout1 = getTypeLinearLayout(0);
+
+        llProductType.addView(linearLayout1);
+        /**
+         * 设置第二行以后的类型数据
+         */
+        if(items.getSku_info().size() > 1) {
+
+            for(int i = 1; i < items.getSku_info().size(); i++) {
+
+                LinearLayout typeLinearLayout = getTypeLinearLayout(i);
+
+
+
+                llProductType.addView(typeLinearLayout);
+            }
+
+        }
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @NonNull
+    private LinearLayout getTypeLinearLayout(int id) {
+        //设置每行类型的标题
+        if(id != 0) {
+            TextView text1 = new TextView(GoodsGotoBuyActivity.this);
+            LinearLayout.LayoutParams lpp1 = new LinearLayout.LayoutParams(//字体高20sp
+                    LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+            text1.setLayoutParams(lpp1);
+            lpp1.setMargins(0,10,0,10);
+
+            text1.setTextColor(Color.parseColor("#C5C5C5"));
+            text1.setTextSize(15);
+            text1.setText(items.getSku_info().get(id).getType_name());
+//                                        textView2.setPadding(0,DensityUtil.dip2px(GoodsDetailsActivity.this,10),0,DensityUtil.dip2px(GoodsDetailsActivity.this,10));
+            llProductType.addView(text1);
+        }
+
+
+        //设置具体没行内容----------------------------------------
+        LinearLayout linearLayout1 = new LinearLayout(this);
+        LinearLayout.LayoutParams lp1 = new LinearLayout
+                .LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT);
+        lp1.setLayoutDirection(LinearLayout.HORIZONTAL);
+        lp1.setMargins(0, 10, 0, 0);
+        linearLayout1.setLayoutParams(lp1);
+        for(int i = 0; i < items.getSku_info().get(id).getAttrList().size(); i++) {
+            List<GoodsDetailsBean.DataBean.ItemsBean.SkuInfoBean.AttrListBean> attrList
+                    = items.getSku_info().get(id).getAttrList();
+
+            TextView text = new TextView(GoodsGotoBuyActivity.this);
+            LinearLayout.LayoutParams lpp = new LinearLayout.LayoutParams(//字体高20sp
+                    LinearLayout.LayoutParams.WRAP_CONTENT, DensityUtil.dip2px(GoodsGotoBuyActivity.this,30));
+            text.setBackgroundColor(Color.parseColor("#424950"));
+            text.setPadding(10,10,10,10);
+            lpp.setMargins(0, 0, 10, 0);//右边距为10dp
+            text.setLayoutParams(lpp);
+
+            text.setTextColor(Color.WHITE);
+            text.setTextSize(12);
+            text.setText(attrList.get(i).getAttr_name());
+//                                        textView2.setPadding(0,DensityUtil.dip2px(GoodsDetailsActivity.this,10),0,DensityUtil.dip2px(GoodsDetailsActivity.this,10));
+            linearLayout1.addView(text);
+        }
+        return linearLayout1;
     }
 
     @Override
