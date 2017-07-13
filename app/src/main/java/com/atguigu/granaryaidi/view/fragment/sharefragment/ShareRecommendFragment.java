98df9166e1b84986d8f0a24cc953f672e53ff306
lianshuideyu <834893479@qq.com>
2017-07-13 20:50:47 +0800

    百思 推荐页面的上下拉刷新

--- app/src/main/java/com/atguigu/granaryaidi/view/fragment/sharefragment/ShareRecommendFragment.java
+++ app/src/main/java/com/atguigu/granaryaidi/view/fragment/sharefragment/ShareRecommendFragment.java
@@ -5,7 +5,9 @@ import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.atguigu.granaryaidi.Base.BaseFragment;
 import com.atguigu.granaryaidi.R;
@@ -14,6 +16,8 @@ import com.atguigu.granaryaidi.common.NetLink;
 import com.atguigu.granaryaidi.utils.HttpUtils;
 import com.atguigu.granaryaidi.view.adapter.share.ShareRecommendAdapter;
 import com.google.gson.Gson;
+import com.jwenfeng.library.pulltorefresh.BaseRefreshListener;
+import com.jwenfeng.library.pulltorefresh.PullToRefreshLayout;
 
 import java.util.List;
 
@@ -30,8 +34,16 @@ public class ShareRecommendFragment extends BaseFragment {
     @InjectView(R.id.progressbar)
     ProgressBar progressbar;
 
+    @InjectView(R.id.pt_fresh)
+    PullToRefreshLayout pt_fresh;
+    @InjectView(R.id.ib_top_share)
+    ImageButton ibTop;
+
     private List<BaisiRecommendBean.ListBean> lists;
     private ShareRecommendAdapter adapter;
+
+    private String url;
+    private int num = 20;
 
     @Override
     public int getLayoutId() {
@@ -41,49 +53,58 @@ public class ShareRecommendFragment extends BaseFragment {
     @Override
     protected void initView() {
 
+        url = NetLink.SHARE_BASE_RECOMMEND + num + ".json";
     }
 
     @Override
     protected void initData() {
-
         /**
          * 联网
          */
-        getDataNet();
+        if (!TextUtils.isEmpty(url)) {
+
+            getDataNet(NetLink.SHARE_RECOMMEND);
+        }
     }
 
     /**
      * 联网
+     *
+     * @param url
      */
-    private void getDataNet() {
+    private void getDataNet(String url) {
+        if (!TextUtils.isEmpty(url)) {
 
-        String url = NetLink.SHARE_RECOMMEND;
+            HttpUtils.getInstance().get(url, new HttpUtils.MyHttpClickListener() {
+                @Override
+                public void onSuccess(String content) {
+                    Log.e("sharere", "联网成功==" + content);
+                    if (!TextUtils.isEmpty(content)) {
+                        //解析数据
+                        processData(content);
 
-        HttpUtils.getInstance().get(url, new HttpUtils.MyHttpClickListener() {
-            @Override
-            public void onSuccess(String content) {
-                Log.e("sharere", "联网成功==" + content);
-                if (!TextUtils.isEmpty(content)) {
-                    //解析数据
-                    processData(content);
-
+                    }
                 }
 
-            }
-
-            @Override
-            public void onFailure(String content) {
-                Log.e("sharere", "联网失败==" + content);
-                progressbar.setVisibility(View.GONE);
-            }
-        });
+                @Override
+                public void onFailure(String content) {
+                    Log.e("sharere", "联网失败==" + content);
+                    progressbar.setVisibility(View.GONE);
+                }
+            });
+        }
     }
 
     /**
      * 解析数据
      */
+    BaisiRecommendBean bean;
+
     private void processData(String content) {
-        BaisiRecommendBean bean = new Gson().fromJson(content, BaisiRecommendBean.class);
+
+
+        bean = new Gson().fromJson(content, BaisiRecommendBean.class);
+
         lists = bean.getList();
 //
         Log.e("sharere", "解析==" + bean.getList().get(0).getU().getName());
@@ -99,14 +120,56 @@ public class ShareRecommendFragment extends BaseFragment {
             LinearLayoutManager manager = new LinearLayoutManager(context);
 
             rlShareRecommend.setLayoutManager(manager);
+            if (isLoadMore) {//判断是上拉还是下拉刷新,isLoadMore为false是为下拉,默认为false
+                //为上拉加载更多
+                rlShareRecommend.scrollToPosition(num - 20);
+            }
         }
-        progressbar.setVisibility(View.GONE);
 
+        progressbar.setVisibility(View.GONE);
+        pt_fresh.finishRefresh();//刷新结束
+        pt_fresh.finishLoadMore();
     }
+
+    private boolean isLoadMore = false;
 
     @Override
     protected void initListener() {
 
+        pt_fresh.setRefreshListener(new BaseRefreshListener() {
+            @Override
+            public void refresh() {
+
+                isLoadMore = false;
+                num = 20;
+                getDataNet(NetLink.SHARE_RECOMMEND);
+
+            }
+
+            @Override
+            public void loadMore() {
+
+                num += 20;
+                isLoadMore = true;
+                if(bean != null && num < bean.getInfo().getCount()) {//小于数据的最大值
+
+                    getDataNet(NetLink.SHARE_BASE_RECOMMEND + num + ".json");
+                }else {
+                    Toast.makeText(context, "没有更多数据", Toast.LENGTH_SHORT).show();
+                    pt_fresh.finishLoadMore();
+                }
+            }
+        });
+
+        /**
+         * 回到顶部图标的点击事件
+         */
+        ibTop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rlShareRecommend.scrollToPosition(0);//回到顶部
+            }
+        });
     }
 
 }
